7e3e1e41e0f647b9cbde0b7c3232ff33d83c5ec5
li cetus <licetus@lideMacBook-Pro.local>
2018-01-11 17:32:25 +0800

    update SimpleTag

--- src/views/public-components/simple-tag/simple-tag.vue
+++ src/views/public-components/simple-tag/simple-tag.vue
@@ -29,21 +29,44 @@ export default {
 	},
 	computed: {
 		style() {
+			let backgroundColor = this.color
+			let fontColor = this.textColor
+			let borderColor = this.color
+			switch (this.color) {
+				case 'blue':
+					backgroundColor = '#2d8cf0'
+					break
+				case 'yellow':
+					backgroundColor = '#f90'
+					break
+				case 'green':
+					backgroundColor = '#19be6b'
+					break
+				case 'red':
+					backgroundColor = '#ed3f14'
+					break
+				case 'default':
+					backgroundColor = '#f7f7f7'
+					borderColor = '#e9eaec'
+					fontColor = '#495060'
+					break
+				default:
+			}
 			return {
 				display: 'inline-block',
 				height: '22px',
 				'line-height': '22px',
 				margin: '2px 4px 2px 0',
 				padding: '0 8px',
-				border: `1px solid ${this.color}`,
+				border: `1px solid ${borderColor}`,
 				'border-radius': '3px',
 				'font-size': '12px',
 				'vertical-align': 'middle',
 				opacity: 1,
 				overflow: 'hidden',
 				cursor: 'pointer',
-				color: this.textColor,
-				background: this.color,
+				color: fontColor,
+				background: backgroundColor,
 			}
 		},
 	},
